3d4efda04dd19372a707174497acbfc18f819714
Kamal19612 <ouedraogokamalelise@gmail.com>
2024-11-05 11:26:45 +0000

    finalisation

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import fonctionnement as ft
 import traitement_fichiers as tf
 
 def main(): 
-    # cette ligne nous permettera 
+    # Attente de l'appui sur la touche "Entrée" pour démarrer
+    input("Appuyez sur la touche 'Entrée' pour commencer...")
+    # cette ligne nous permettera scanner les interfaces
     list_interfaces = ft.scanner()
     # la fonction choix_port permettera de scanner les interfaces, a l'utilisateur de faire son choix et de recuperer le choix fait par la variable port
     port=ft.choix_port(list_interfaces)
--- traitement_fichiers.py
+++ traitement_fichiers.py
@@ -173,11 +173,6 @@ def statistic(df):
         plt.show(block=False)  # Affiche le graphique dans une nouvelle fenêtre
     
         # Graphique 3 : Répartition des types de requête
-        # plt.figure(figsize=(8, 6))
-        # sns.countplot(x='Request Type', data=df)
-        # plt.title('Répartition des types de requête')
-        # plt.show(block=False)  # Affiche le graphique dans une nouvelle fenêtre
-        # Comptage des occurrences par type de requête
         request_counts = df['Request Type'].value_counts()
         labels = request_counts.index
         sizes = request_counts.values
